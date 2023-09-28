d53641401b1b5b06bae7d2ca09b33c79a9a2ac4c
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-09-29 00:10:38 +0200

    added dashboard layout

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import firebase from './config/fire.js';
 import './Home.css';
 import Signout from './Signout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faChartBar, faCogs, faQuestionCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 export default class Home extends Component {
   constructor(props) {
@@ -11,6 +14,12 @@ export default class Home extends Component {
       timer: 8 * 60 * 60, // 8 hours in seconds
     };
   }
+
+  toggleSidePanel = () => {
+    this.setState((prevState) => ({
+      sidePanelClosed: !prevState.sidePanelClosed,
+    }));
+  };
 
   componentDidMount() {
     this.timerInterval = setInterval(this.updateTimer, 1000);
@@ -39,35 +48,43 @@ export default class Home extends Component {
 
   render() {
     return (
-        <div class="home-container">
-            <header class="header">
-                <h1>ChronoLogix</h1>
-                <h3>Empowering Efficiency, Elevating Time.</h3>
-            </header>
-    
-            <section class="welcome-section">
-                <h3>Welcome Back!</h3>
-                <h6 class="welcome-text">You have successfully signed in with ChronoLogix. Your time is now being tracked.</h6>
-                <h6 class="additional-text">Make the most of your productive hours. Happy and efficient working!</h6>
-            </section>
-
-            <div class="timer-section">
-                <p class="time-remaining">Time remaining: {this.formatTime(this.state.timer)}</p>
-            </div>
-
-            <div class="hero-image">
-                <img src="countdown.svg" alt="hero-img" width="300" height="300"/>
-            </div>
-
-            <div>
-              <button class="logout-button" onClick={this.logout}>Logout</button>
-            </div>
-            
-            <section class="support-section">
-                <p class="support-text">Our <a class="link" href="about.html">support</a> team is ready to assist with any queries or needs you may have,<a class="link" href="Contact.html"> contact us</a>.</p>
-                <p class="success-text">Clock in, excel, and let every moment contribute to your success!</p>
-            </section>           
-        </div>         
+      <div class="dashboard-container">
+        <div class="side-panel">
+        <div class="logo">
+          <h1>ChronoLogix</h1>
+        </div>
+       <ul class="menu">
+        <li><a href="#"><i class="fas fa-cogs"></i> Notifications</a></li>
+          <li><a href="#"><i class="fas fa-chart-bar"></i> Analytics</a></li>
+          <li><a href="#"><i class="fas fa-cogs"></i> Timesheet</a></li>
+          <li><a href="#"><i class="fas fa-cogs"></i> Profile</a></li>
+          <li><a href="#"><i class="fas fa-cogs"></i> Settings</a></li>
+          <li><a href="#"><i class="fas fa-question-circle"></i> Support</a></li>
+          <li><a href="#"><i class="fas fa-cogs"></i> FAQ</a></li>
+          <button class="logout-button" onClick={this.logout}>Logout</button>
+        </ul>
+      </div>
+      <div class="main-content">
+          <div class="home-container">
+              <header class="header">
+                  <h1>ChronoLogix</h1>
+                  <h3>Empowering Efficiency, Elevating Time.</h3>
+              </header>
+              <section class="welcome-section">
+                  <h3>Welcome Back!</h3>
+                  <p class="welcome-text">You have successfully signed in with ChronoLogix. Your time is now being tracked.</p>
+              </section>
+              <div class="timer-section">
+                  <p class="time-remaining">Time remaining: {this.formatTime(this.state.timer)}</p>
+              </div>
+              <div class="hero-image">
+                  <img src="countdown.svg" alt="hero-img" width="300" height="300"/>
+                  <p class="additional-text">Make the most of your productive hours. Happy and efficient working!</p>
+              </div>
+          </div>
+      </div>
+  </div>
+           
     );
   }
 }
